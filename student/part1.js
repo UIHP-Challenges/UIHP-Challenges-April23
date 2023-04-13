0ead23c3efc965733f536b471bae44db62c18952
Emilia Yoffie <emiliayoffie@arcane-air.lan>
2023-04-13 19:09:52 -0400

    updates + added rough draft of part 5 readme

--- student/part1.js
+++ student/part1.js
@@ -1,1 +1,14 @@
-//
+let post = '';
+
+const jsInput = document.querySelector('input');
+const jsDiv = document.querySelector('div');
+
+function handleInput(){
+    post = jsInput.value
+    jsDiv.textContent = post 
+};
+
+jsInput.oninput = handleInput;
+
+// How do I get started? refactor intro
+
